ff4a5a3488c7147a7ce4effa2c9ec4ed214de239
Poliana <polibarross@gmail.com>
2025-03-06 16:56:41 -0300

    Initial commit

---
+++ src/main/java/me/dio/avanade_estudos_dio/AvanadeEstudosDioApplication.java
@@ -0,0 +1,13 @@
+package me.dio.avanade_estudos_dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvanadeEstudosDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvanadeEstudosDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/avanade_estudos_dio/AvanadeEstudosDioApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.avanade_estudos_dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvanadeEstudosDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
